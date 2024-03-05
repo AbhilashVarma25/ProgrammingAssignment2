75918b35717e93c3b3539d50384fd4b0a038dc0f
AbhilashVarma25 <145323531+AbhilashVarma25@users.noreply.github.com>
2024-03-05 12:43:04 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# Define a function to cache potentially time-consuming computations
+make_cache <- function(f) {
+  cache <- NULL
+  
+  # Define a function that checks if the result is cached
+  cached_function <- function(...) {
+    args <- list(...)
+    arg_string <- paste(args, collapse = "_")
+    
+    if (!is.null(cache) && identical(cache$args, args)) {
+      message("Using cached result.")
+      return(cache$result)
+    } else {
+      result <- f(...)
+      cache <<- list(args = args, result = result)
+      return(result)
+    }
+  }
+  
+  return(cached_function)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# Example usage:
+# Define a function for a potentially time-consuming computation
+compute_mean <- function(x) {
+  mean(x)
 }
+
+# Create a cached version of the function
+cached_mean <- make_cache(compute_mean)
+
+# Call the cached function
+cached_mean(c(1, 2, 3, 4, 5))  # This will compute the mean
+cached_mean(c(1, 2, 3, 4, 5))  # This will use the cached result
